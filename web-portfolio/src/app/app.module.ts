1303f1435d57851e3e8396df28724a33ab98b4ed
Ashley Robbins <robbinsa1@central.edu>
2023-02-02 21:18:40 -0600

    added routing wip: buttons being responsive

--- web-portfolio/src/app/app.module.ts
+++ web-portfolio/src/app/app.module.ts
@@ -4,17 +4,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ResumeComponent } from './resume/resume.component';
+import { CodeProjectsComponent } from './code-projects/code-projects.component';
+import { CreativeCollectionComponent } from './creative-collection/creative-collection.component';
+import { BackstoryComponent } from './backstory/backstory.component';
+import { RouterModule } from "@angular/router";
 
 //decorator that converts ts to angular
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ResumeComponent,
+    CodeProjectsComponent,
+    CreativeCollectionComponent,
+    BackstoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path: '', component: LandingPageComponent},
+      {path: 'resume', component: ResumeComponent},
+      {path: 'code-projects', component: CodeProjectsComponent},
+      {path: 'creative-collection', component: CreativeCollectionComponent},
+      {path: 'backstory', component: BackstoryComponent}
+    ])
   ],
   //providers = dependencies for this module (allows for injection) --services go here
   providers: [
---
+++ web-portfolio/src/app/code-projects/code-projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-projects',
+  templateUrl: './code-projects.component.html',
+  styleUrls: ['./code-projects.component.css']
+})
+export class CodeProjectsComponent {
+
+}
